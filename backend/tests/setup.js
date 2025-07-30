2007160b3faf4416136696aea37b3449487a5253
NikeshDev-LF <nikeshdevkota@lftechnology.com>
2025-07-30 14:39:12 +0545

    Backend test implementation

--- backend/tests/setup.js
+++
@@ -1,9 +0,0 @@
-const pool = require('../src/config/database');
-
-// Global teardown to close database connections
-afterAll(async () => {
-  await pool.end();
-});
-
-// Increase timeout for database operations
-jest.setTimeout(15000); 
